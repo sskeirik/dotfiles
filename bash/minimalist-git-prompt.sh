84731919d58f30db2f7648448d77767e7459fced
Stephen Skeirik <skeirik2@illinois.edu>
2018-10-28 20:10:22 -0500

    bash: fix minimal git prompt quoting + formatting

--- bash/minimalist-git-prompt.sh
+++ bash/minimalist-git-prompt.sh
@@ -1,9 +1,8 @@
-RESET=$(tput sgr0)
-
-BLUE=$(tput setaf 4)
-GREY=$(tput setaf 244)
-RED=$(tput setaf 1)
-YELLOW=$(tput setaf 3)
+RESET="$(tput sgr0)"
+BLUE="$(tput setaf 4)"
+GREY="$(tput setaf 244)"
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
 
 git_prompt() {
   BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')
@@ -11,12 +10,12 @@ git_prompt() {
   if [ ! -z $BRANCH ]; then
     echo -n "$YELLOW$BRANCH"
 
-    if [ ! -z "$(git status --short)" ]; then
+    if [ ! -z "$(git status --porcelain=v1)" ]; then
       echo " ${RED}✗"
     fi
   fi
 }
 
 PS1='
-$BLUE\w$(git_prompt)
-$GREY$ $RESET'
+\[$BLUE\]\w$(git_prompt)
+\[$GREY\]$ \[$RESET\]'
