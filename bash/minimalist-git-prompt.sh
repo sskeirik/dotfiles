ec4e0bcf4cf3763d7c365af8a69e3c7ed025f0b0
Stephen Skeirik <skeirik2@illinois.edu>
2018-10-29 19:32:07 -0500

    bash: make git prompt script more robust

--- bash/minimalist-git-prompt.sh
+++ bash/minimalist-git-prompt.sh
@@ -1,3 +1,8 @@
+# Note: if colors aren't working, check:
+# 1) value of $TERM variable outside of tmux session
+# 2) value of $TERM variable inside  of tmux session
+# 3) value of terminal-overrides in tmux configuration
+
 RESET="$(tput sgr0)"
 BLUE="$(tput setaf 4)"
 GREY="$(tput setaf 244)"
@@ -5,12 +10,12 @@ RED="$(tput setaf 1)"
 YELLOW="$(tput setaf 3)"
 
 git_prompt() {
-  BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')
+  BRANCH="$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/*\(.*\)/\1/')"
 
-  if [ ! -z $BRANCH ]; then
+  if [ ! -z "$BRANCH" ]; then
     echo -n "$YELLOW$BRANCH"
 
-    if [ ! -z "$(git status --porcelain=v1)" ]; then
+    if [ ! -z "$(git status --porcelain=v1 2> /dev/null)" ]; then
       echo " ${RED}✗"
     fi
   fi
