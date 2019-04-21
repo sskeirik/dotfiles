d1de61003cec8996805991957e38ea34fc5672eb
Stephen Skeirik <skeirik2@illinois.edu>
2019-04-21 15:26:28 -0500

    meta: update link.sh for zsh and bash compatibility

--- meta/link.sh
+++ meta/link.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-ln -s $HOME/.config/bash/bashrc  $HOME/.bashrc
-ln -s $HOME/.config/bash/inputrc $HOME/.inputrc
+# if we are not ZSH, assume BASH
+if [ -z "$ZSH_VERSION" ]; then
+  ln -s $HOME/.config/shell/shellrc  $HOME/.bashrc
+  ln -s $HOME/.config/bash/inputrc   $HOME/.inputrc
+# otherwise, ZSH
+else
+  ln -s $HOME/.config/shell/shellrc  $HOME/.zshrc
+fi
