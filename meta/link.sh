6e3465ca93dab5686bd1edb7bf8cb661dba56317
Stephen Skeirik <skeirik2@illinois.edu>
2019-05-14 12:10:25 -0500

    display: add initial display configuration files

--- meta/link.sh
+++ meta/link.sh
@@ -12,6 +12,10 @@ fi
 # if full Linux installation is requested
 if [ "$1" = "full" ]; then
   ln -s $HOME/.config/sound/default-asoundrc $HOME/.asoundrc
+  ln -s $HOME/.config/display/xinitrc        $HOME/.xinitrc
+  echo "For High-DPI screens, please make sure browsers are scaled properly"
+  echo "For chromium: check config/display/chromium-flags.conf"
+  echo "For firefox: do online search"
 else
   echo "Console installations will not link graphics/sound config files"
   echo "For full installation, re-run with as:"
