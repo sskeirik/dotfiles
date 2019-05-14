d78d374dc8f88734aba50fc943058e872ee04a5f
Stephen Skeirik <skeirik2@illinois.edu>
2019-05-14 12:45:05 -0500

    meta: fix bug in installation script for bash

--- meta/link.sh
+++ meta/link.sh
@@ -3,7 +3,7 @@
 # if we are not ZSH, assume BASH
 if [ -z "$ZSH_VERSION" ]; then
   ln -s $HOME/.config/shell/shellrc  $HOME/.bashrc
-  ln -s $HOME/.config/bash/inputrc   $HOME/.inputrc
+  ln -s $HOME/.config/shell/inputrc  $HOME/.inputrc
 # otherwise, ZSH
 else
   ln -s $HOME/.config/shell/shellrc  $HOME/.zshrc
