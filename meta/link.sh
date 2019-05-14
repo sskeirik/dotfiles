f804e8640abfe702fc299ae055fe9f6869411026
Stephen Skeirik <skeirik2@illinois.edu>
2019-05-14 13:29:03 -0500

    meta: fix install script shell selection; improve output

--- meta/link.sh
+++ meta/link.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
+# Config file installation script
 
-# if we are not ZSH, assume BASH
-if [ -z "$ZSH_VERSION" ]; then
-  ln -s $HOME/.config/shell/shellrc  $HOME/.bashrc
-  ln -s $HOME/.config/shell/inputrc  $HOME/.inputrc
-  ln -s $HOME/.config/shell/profile  $HOME/.bash_profile
+# shell selection
+if [ "$1" = "bash" ]; then
+  echo "bash selected; installing bash links..."
+  ln -s $HOME/.config/shell/shellrc  $HOME/.bashrc       2>/dev/null
+  ln -s $HOME/.config/shell/inputrc  $HOME/.inputrc      2>/dev/null
+  ln -s $HOME/.config/shell/profile  $HOME/.bash_profile 2>/dev/null
 # otherwise, ZSH
+elif [ "$1" = "zsh" ]; then
+  echo "zsh selected, installing zsh links..."
+  ln -s $HOME/.config/shell/shellrc  $HOME/.zshrc    2>/dev/null
+  ln -s $HOME/.config/shell/profile  $HOME/.zprofile 2>/dev/null
 else
-  ln -s $HOME/.config/shell/shellrc  $HOME/.zshrc
-  ln -s $HOME/.config/shell/profile  $HOME/.zprofile
+  echo "usage: link.sh (bash|zsh) [full]"
+  exit 1
 fi
 
+echo "" # separate output
+
 # if full Linux installation is requested
-if [ "$1" = "full" ]; then
-  ln -s $HOME/.config/sound/default-asoundrc $HOME/.asoundrc
-  ln -s $HOME/.config/display/xinitrc        $HOME/.xinitrc
+if [ "$2" = "full" ]; then
+  ln -s $HOME/.config/sound/default-asoundrc $HOME/.asoundrc 2>/dev/null
+  ln -s $HOME/.config/display/xinitrc        $HOME/.xinitrc  2>/dev/null
   echo "For High-DPI screens, please make sure browsers are scaled properly"
   echo "For chromium: check config/display/chromium-flags.conf"
   echo "For firefox: do online search"
 else
   echo "Console installations will not link graphics/sound config files"
   echo "For full installation, re-run with as:"
-  echo "$ link.sh full"
+  echo "$ link.sh $1 full"
 fi
