c7e3250052e0568c165ee52f17955aa422577361
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2023-11-01 15:35:24 -0400

    container: improve ubuntu bring-up script

--- container/bring-up-ubuntu-container.sh
+++ container/bring-up-ubuntu-container.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 # bring-up-ubuntu-container.sh
+set -euo pipefail
 
+USERS=$(cat /etc/passwd | grep -vE "nologin|false" | cut -f1 -d:)
 ME=stephen
-
-# set passwd
-read -p "Enter your password: " -s pw
-echo
-read -p "Enter your password again: " -s pw2
-[[ "$pw" != "$pw2" ]] && echo "Passwords do not match" && exit 1
-
-packages=(                                     \
-  python3 python3-dev python3-pip python3-venv \
-  npm                                          \
-  opam                                         \
+PKGS=(                                         \
+  python3-dev python3-pip python3-venv         \
   curl wget openssl ca-certificates            \
-  libsecp256k1-dev libsodium-dev               \
   git                                          \
   sudo                                         \
   man-db                                       \
-  neovim                                       \
+  gpg-agent software-properties-common         \
+  ssh                                          \
 )
 
-# unminimize system to get man-db
-# echo -e "Y\nY\n" | unminimize
+# create user if needed
+if grep -E "^$ME\$" &>/dev/null <<< $USERS; then
+  echo "User exists. Continuing..."
+else
+  echo "User does not exist. Creating..."
+
+  # set passwd
+  read -p "Enter your password: " -s pw
+  echo
+  read -p "Enter your password again: " -s pw2
+  [[ "$pw" != "$pw2" ]] && echo "Passwords do not match" && exit 1
+
+  # create user and set passwds
+  useradd -m -G sudo -s /bin/bash "$ME"
+  echo -e "$pw\n$pw" | passwd "$ME" &>/dev/null
+fi
+
+# echo commands to be performed
+set -x
 
 # remove man pages from excluded files
 sed -i 's@^path-exclude=/usr/share/man/@#path-exclude=/usr/share/man/@' /etc/dpkg/dpkg.cfg.d/excludes
@@ -30,17 +40,15 @@ sed -i 's@^path-exclude=/usr/share/man/@#path-exclude=/usr/share/man/@' /etc/dpk
 # run non-interactive, minimal installation
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get --no-install-recommends --assume-yes install ${packages[@]}
+apt-get --no-install-recommends --assume-yes install "${PKGS[@]}"
+add-apt-repository --yes ppa:neovim-ppa/stable
+apt-get update
+apt-get --no-install-recommends --assume-yes install neovim
 unset DEBIAN_FRONTEND
-
-# create user and set passwds
-echo -e "$pw\n$pw" | passwd
-useradd -m -G sudo -s /bin/bash "$ME"
-echo -e "$pw\n$pw" | passwd "$ME"
 
 # install non-packaged binaries
 
 # install non-packaged binaries as user
-su - stephen << EOF
+su - "$ME" << EOF
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 EOF
