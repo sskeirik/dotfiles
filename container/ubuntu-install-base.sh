5c286586bd31f607a29b13a4f94b71ce273b3ca5
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2023-11-17 19:18:08 +0000

    container: improve ubuntu bring-up

--- container/ubuntu-install-base.sh
+++ container/ubuntu-install-base.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
+NEOVIM_PPA=neovim-ppa/unstable
+
 # define packages to be installed
 PKGS=(                                         \
+  build-essential                              \
   gpg-agent software-properties-common         \
   sudo                                         \
-  man-db                                       \
+  docker.io                                    \
   python3-dev python3-pip python3-venv         \
   curl wget openssl ca-certificates            \
   git                                          \
   ssh                                          \
   unzip                                        \
-  htop tree ripgrep fd-find direnv             \
+  zsh file htop tree ripgrep fd-find direnv    \
 )
 # define extras to be installed
 EXTRAS=(         \
+  setup-man      \
   neovim         \
   rename-fd-find \
 )
+
+function update() {
+  DEBIAN_FRONTEND=noninteractive apt-get update --quiet
+}
+function install() {
+  DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends --yes --quiet "$@"
+}
+function reinstall() {
+  DEBIAN_FRONTEND=noninteractive apt-get reinstall --no-install-recommends --yes --quiet "$@"
+}
 
 if [ $EUID -ne 0 ]; then
   echo -e "Please run as root"
@@ -27,15 +41,36 @@ fi
 # echo commands to be performed
 set -x
 
-# remove man pages from excluded files
-sed -i 's@^path-exclude=/usr/share/man/@#path-exclude=/usr/share/man/@' /etc/dpkg/dpkg.cfg.d/excludes
+# install manpages
+sed -i 's:^path-exclude=/usr/share/man:#path-exclude=/usr/share/man:' /etc/dpkg/dpkg.cfg.d/excludes
+update
+install             \
+  man               \
+  manpages          \
+  manpages-dev      \
+  manpages-posix    \
+  manpages-posix-dev
+
+# finish installing man
+rm -f /usr/bin/man
+dpkg-divert --quiet --remove --rename /usr/bin/man
+rm -f /usr/share/man/man1/sh.1.gz
+dpkg-divert --quiet --remove --rename /usr/share/man/man1/sh.1.gz
+
+# setup apt basics
+install apt-utils dialog
+
+# reinstall apt to get manpage
+reinstall apt
+
+# setup timezone and locale
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+install locales
+sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && locale-gen
 
 # run non-interactive, minimal installation
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get --no-install-recommends --assume-yes install "${PKGS[@]}"
-add-apt-repository --yes ppa:neovim-ppa/stable
-apt-get update
-apt-get --no-install-recommends --assume-yes install neovim
+install "${PKGS[@]}"
+( add-apt-repository -L | rg "$NEOVIM_PPA" ) || { add-apt-repository --yes ppa:"$NEOVIM_PPA" ; update }
+install neovim
 ln -s $(which fdfind) ~/.local/bin/fd
 unset DEBIAN_FRONTEND
