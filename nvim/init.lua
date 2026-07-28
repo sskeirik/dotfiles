dfda22b763c9a4dece524d25be4e0a87a0fc5709
Stephen Skeirik <stephen.skeirik@pi2.network>
2026-07-28 16:44:08 -0400

    nvim: update vimtex plugin, disable tree-sitter for latex

--- nvim/init.lua
+++ nvim/init.lua
@@ -260,6 +260,11 @@ require'nvim-treesitter.configs'.setup {
     -- disable = { "c", "rust" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
+        local disabled_langs = { "latex" }
+        if vim.tbl_contains(disabled_langs, lang) then
+            return true
+        end
+
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
